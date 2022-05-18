11d86a2038198aa77a158018f2e6f588b718ed3a
theMillenniumFalcon <nishankpr2002@gmail.com>
2022-05-18 16:46:05 +0530

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components'
+
+const Component1 = styled.div`
+font-family: galanogrotesque-m;
+`;
+
+const Component2 = styled.div`
+font-family: galanogrotesque-r;
+`;
+
+const Component3 = styled.div`
+font-family: galanogrotesque-s;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Component1>
+        Hello
+      </Component1>
+      <Component2>
+        Hello
+      </Component2>
+      <Component3>
+        Hello
+      </Component3>
+    </>
+
   );
 }
 
