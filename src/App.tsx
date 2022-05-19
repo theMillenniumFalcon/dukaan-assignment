f7f4c5778a5ea3b8b053d1124986d82d9abb2f90
theMillenniumFalcon <nishankpr2002@gmail.com>
2022-05-19 23:45:26 +0530

    chaged something

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,24 @@
+import styled from 'styled-components';
 import { Footer } from './components/footer/Footer';
 import { FreeProductsSection } from './components/freeproductssection/FreeProductsSection';
 import { SloganSection } from './components/slogansection/SloganSection';
 import { ThreeOptionSection } from './components/threeoptionsection/ThreeOptionSection';
 import { TopSection } from './components/topsection/TopSection';
 
+const AppContainer = styled.div`
+/* position: relative; */
+background-color: #FAFAFA;
+`;
+
 function App() {
   return (
-    <>
+    <AppContainer>
       <TopSection />
       <SloganSection />
       <ThreeOptionSection />
       <FreeProductsSection />
       <Footer />
-    </>
+    </AppContainer>
   );
 }
 
--- src/components/slogansection/SloganSection.tsx
+++ src/components/slogansection/SloganSection.tsx
@@ -1,19 +1,27 @@
 import React from "react";
 import styled from "styled-components"
-import { SloganCard } from "../slogancard/SloganCard";
+import { SloganDisplay } from "../slogandisplay/SloganDisplay";
+import { SloganSectionHeader } from "../slogansectionHeader/SloganSectionHeader";
+import { Wrapper } from "../wrapper/Wrapper";
 
 interface SloganSectionProps { }
 
-const SloganSectionComponent = styled.div`
-background-color: #FAFAFA;
-padding-bottom: 3em;
-margin-bottom: 0px;
+const SloganSectionContainer = styled.div`
+background-color: #FFFFFF;
+border-radius: 8px;
+box-shadow: rgba(26, 24, 30, 0.06);
+width: 100%;
+height: 80%;
+z-index: 99999;
 `;
 
 export const SloganSection: React.FC<SloganSectionProps> = () => {
     return (
-        <SloganSectionComponent>
-            <SloganCard />
-        </SloganSectionComponent>
+        <Wrapper>
+            <SloganSectionContainer>
+                <SloganSectionHeader />
+                <SloganDisplay />
+            </SloganSectionContainer>
+        </Wrapper>
     )
 }
